d30ef96112c6397e527dcdbe71a2d03e9fd5a293
Ahmet Ozer <cagriahmet32@gmail.com>
2023-03-26 18:07:47 -0400

    Initial Backend Setup

---
+++ backend/src/main/java/devs/backend/App.java
@@ -0,0 +1,13 @@
+package devs.backend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
